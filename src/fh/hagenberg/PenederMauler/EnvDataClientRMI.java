1128fea39cbe86a30caf0749c2e9ec79b7ea0ee2
moly94 <felix.mauler@gmail.com>
2016-12-21 11:10:50 +0100

    Added new Classes

--- src/fh/hagenberg/PenederMauler/EnvDataClientRMI.java
+++ src/fh/hagenberg/PenederMauler/EnvDataClientRMI.java
@@ -12,20 +12,20 @@ public class EnvDataClientRMI {
         this.envData = envData;
     }
 
+    public IEnvironmentData getEnvData() {
+        return envData;
+    }
+
+    public void setEnvData(IEnvironmentData envData) {
+        this.envData = envData;
+    }
+
     public static void main (String[] _argv) {
         try {
 
             String adr = "EnvDataServer";
             IEnvironmentData environmentData= (IEnvironmentData) Naming.lookup(adr);
 
-            String[] dataTypes = envData.requestEnvironmentDataTypes();
-            EnvData[] allEnvData;
-            System.out.println("All sensortypes: ");
-            for (String type : dataTypes) {
-                System.out.print(", " + type);
-            }
-            System.out.println();
-            allEnvData = envData.requestAll();
             System.out.println("Requesting air pressure: ");
             EnvData airData = envData.requestEnvironmentData("air");
 
